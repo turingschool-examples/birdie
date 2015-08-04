cfd31f80bbbf6b3d1689fa82f5a2ed2f2f28cc54
novohispano <novohispano@gmail.com>
2015-08-04 12:19:13 -0600

    Fix order and sanitize user inputs

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+  include ActionView::Helpers::SanitizeHelper
+
+  default_scope { order(:created_at) }
+
+  before_validation :sanitize_description
+
+  def sanitize_description
+    self.description = strip_tags(description)
+  end
 end
